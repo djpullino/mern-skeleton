c7672b65d287f43ffb4c9a52b1bdad135fce1bcb
djpullino <djpullino6503@gmail.com>
2023-03-02 09:35:14 -0500

    starting to make function that filters stops, displays only accessible stops by default

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -14,9 +14,6 @@ const HomePage = () => {
         setUser(getUserInfo())
     }, [])
 
-    
-
-
     if (!user) return (
         <div><h4>Log in to view this page.</h4></div>)
     const { id, email, username, password } = user
--- frontend/src/components/pages/trainStops.js
+++ frontend/src/components/pages/trainStops.js
@@ -3,73 +3,67 @@ import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
 function TrainStops() {
-  const [stops, setAlerts] = useState([]);
+  const [stops, setStops] = useState([]);
+  const [accessibleStops, setAccessibleStops] = useState([]);
+  const [selectedLine, setSelectedLine] = useState('');
+  const [validLines, setValidLines] = useState([]);
 
   var link = document.createElement('link');
-link.setAttribute('rel', 'stylesheet');
-link.setAttribute('type', 'text/css');
-link.setAttribute('href', "https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
-document.head.appendChild(link);
-
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('href', "https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+  document.head.appendChild(link);
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios(
-        'https://api-v3.mbta.com/stops',
-      );
-      setAlerts(result.data.data);
+      const result = await axios('https://api-v3.mbta.com/stops');
+      const filteredStops = result.data.data.filter(stop => stop.attributes.wheelchair_boarding === 1);
+      setStops(filteredStops);
+      setAccessibleStops(filteredStops);
     }
     fetchData();
+
+    async function fetchRoutes() {
+      const result = await axios('https://api-v3.mbta.com/routes');
+      const validRoutes = result.data.data.map(route => route.attributes.long_name);
+      setValidLines(validRoutes);
+    }
+    fetchRoutes();
   }, []);
 
-  function submit(){
-    var userStops = document.getElementById('stopsTest')
-  
+  function submit(event) {
+    event.preventDefault();
+    const userStops = document.getElementById('stopsTest').value.toLowerCase();
+    const filteredStops = stops.filter(stop =>
+      stop.attributes['wheelchair_boarding'] === 1 &&
+      stop.relationships.route.data &&
+      stop.relationships.route.data?.attributes?.long_name?.toLowerCase() === userStops.toLowerCase()
+    );
+    setAccessibleStops(filteredStops);
+    setSelectedLine(userStops);
   }
 
-  
-
   return (
-
-    
     <div>
-      
-    <div style={{textAlign: 'center', paddingTop: "10px", fontFamily: 'Montserrat'}}>
-      <form>
-        Choose a Line to see its Accessible Stops: <input type="text" id="stopsTest"></input>
-      </form>
-      <button onclick="submit();" id="button1" type="button">Enter</button>
+      <div style={{ textAlign: 'center', paddingTop: "10px", fontFamily: 'Montserrat' }}>
+        <label htmlFor="stopsTest">Choose a Line to see its Accessible Stops:</label>
+        <input type="text" id="stopsTest" />
+        <button onClick={submit} type="button">Enter</button>
       </div>
-      {/* {stops.map(stops => (
-        <Card
-        body
-        outline
-        color="success"
-        className="mx-1 my-2"
-        style={{ width: "30rem" }}
-      >
-        <Card.Body>
-        <Card.Title>Stops</Card.Title>
-        <Card.Text>{stops.attributes.header}{stops.attributes.description}</Card.Text>
-        </Card.Body>
-      </Card>
-      ))} */}
 
-    
+      <h1 style={{ textAlign: 'center', paddingTop: "10px", fontFamily: 'Montserrat' }}>
+        {selectedLine ? `Accessible Stops for ${selectedLine} Line` : 'All Accessible Stops'}
+      </h1>
 
-     
-
-        <h1 style={{textAlign: 'center', paddingTop: "10px", fontFamily: 'Montserrat'}}>Stops</h1>
-      {stops.map(stops => (
-        <div key={stops.id} style={{paddingLeft: "10px", textAlign: 'center', fontFamily: 'Montserrat'}}>
-          <h3  >{stops.attributes.header}</h3>
-          <p>{stops.attributes.description}</p>
-          </div>
+      {accessibleStops.map(stop => (
+        <div key={stop.id} style={{ paddingLeft: "10px", textAlign: 'center', fontFamily: 'Montserrat' }}>
+          <h3>{stop.attributes.header}</h3>
+          <p>{stop.attributes.description}</p>
+        </div>
       ))}
     </div>
-    
   );
-  
 }
 
 export default TrainStops;
+
