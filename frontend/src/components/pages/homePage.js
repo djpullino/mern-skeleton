0bdabe9032ccc992c341377bf95f6e275e729702
chrisSmithCoding <heychrisgs@gmail.com>
2023-03-24 12:18:16 -0400

    Added a mbta route image and centered the card

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import getUserInfo from '../../utilities/decodeJwt'
+import Card from 'react-bootstrap/Card';
 const HomePage = () => {
     const [user, setUser] = useState({})
     const navigate = useNavigate()
@@ -22,24 +23,30 @@ const HomePage = () => {
             <div>
                 <h3>
                     Welcome
+                </h3>
                     <span className='username'> @{username}</span>
-                </h3>
+                
                 <h3>
-                    Your userId in mongo db is
+                    Your userId in mongo db is:
+                </h3>
                     <span className='userId'> {id}</span>
-                </h3>
+                
                 <h3>
-                    Your registered email is
+                    Your registered email is: 
+                </h3>
                     <span className='email'> {email}</span>
-                </h3>
+                
                 <h3>
-                    Your password is
+                    Your password is:
+                </h3>
+
                     <span className='password'> {password} ( hashed )</span>
-                </h3>
+                
                 <h3>
-                    Your favorite route is 
-                    <span className='favroute'> {favroute}</span>
+                    Your favorite route is currently set as:
                 </h3>
+                    <span className='favroute'> {favroute}</span>
+                
             </div>
             <button onClick={(e) => handleClick(e)}>
                 Log Out
--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -1,19 +1,22 @@
 import React, {} from 'react'
 import Card from 'react-bootstrap/Card';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 const Landingpage = () => {
     
     return (
-        <Card style={{ width: '30rem' }} className="mx-2 my-2">
+        <div className="d-flex justify-content-center">
+        <Card style={{ width: '40rem' }} className="mx-20 my-20"> 
+        <Card.Img src ="https://external-preview.redd.it/KWkc0wreGafiCXOXC2ymxUv3qmoHTwK6cDZLOwxiBIg.png?auto=webp&s=97c093a33170d7c1f7b5250debe109aaacba3ce5" />
         <Card.Body>
-          <Card.Title>Professor Brockenbrough's User Skeleton App</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">A starting point for an application.</Card.Subtitle>
+          <Card.Title>MBTA Disabled Accesible Stops</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">An easy to understand webpage that displays disabled accesible trainstops</Card.Subtitle>
           <Card.Text>
           </Card.Text>
           <Card.Link href="/signup">Sign Up</Card.Link>
           <Card.Link href="/login">Login</Card.Link>
         </Card.Body>
       </Card>
+      </div>
     )
 }
 
