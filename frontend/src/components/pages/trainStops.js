d10a099fd01c66f829ff9c9caae6097bb2a680c0
djpullino <djpullino6503@gmail.com>
2023-04-06 09:46:51 -0400

    fixed styling, fixed dropdown

--- frontend/src/components/pages/trainStops.js
+++ frontend/src/components/pages/trainStops.js
@@ -17,9 +17,9 @@ function TrainStops() {
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios('https://api-v3.mbta.com/stops');
+      const result = await axios('https://api-v3.mbta.com/stops?filter[route]=Red,Blue,Green-B,Green-C,Green-D,Green-E,Mattapan,Orange');
       const filteredStops = result.data.data.filter(stop => stop.attributes.wheelchair_boarding === 1);
-      setStops(filteredStops);
+      setAccessibleStops(filteredStops);
        //filters all stops to be ones that are accessible
     }
     fetchData();
@@ -51,7 +51,7 @@ function handleSelect(event) {
         <label htmlFor="stopsTest">Choose a Line to see its Accessible Stops:</label> 
         <br></br>
         <select style={{backgroundColor: '#0c0c1f', color: 'white', textAlign: 'center'}}id="stopsTest" value={selectedLine} onChange={handleSelect}>
-          <option value="">Choose a Boston Line</option>
+          <option value="" disabled>Choose a Boston Line</option>
           <option value="Red">Red Line</option>
           <option value="Orange">Orange Line</option>
           <option value="Blue">Blue Line</option>
@@ -68,14 +68,18 @@ function handleSelect(event) {
   {selectedLine ? `Accessible Stops for ${selectedLine} Line`: 'All Accessible Stops'}
       </h1>
 
-      <h2 id="optionPrompt" style={{textAlign:'center', fontFamily: 'Montserrat', paddingBottom: 1000}}>Pick an Option from the Menu and Press the Enter Button</h2>
+      <h2 id="optionPrompt" style={{textAlign:'center', fontFamily: 'Montserrat', fontSize: '25px'}}>Pick an Option from the Menu and Press the Enter Button</h2>
       {accessibleStops.map(stop => (
         <div key={stop.id} style={{ paddingLeft: "10px", textAlign: 'center', fontFamily: 'Montserrat'}}>
           <h3>{stop.attributes.name}</h3>
           <p>{stop.attributes.address}</p>
         </div>
       ))}
+
+      <h5 style={{fontFamily: 'Montserrat', textAlign: 'center'}}><a href='https://www.mbta.com/developers/v3-api' target='_blank' style={{color: 'white'}}>Stops are displayed via the MBTA V3 API Here</a></h5>
+      
     </div>
+
   );
 }
 
