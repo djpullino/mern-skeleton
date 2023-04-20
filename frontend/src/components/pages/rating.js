e3bbd74bc9853d738fc3aa03f267b826f32718ce
djpullino <djpullino6503@gmail.com>
2023-04-20 09:38:06 -0400

    reverted ratings

--- frontend/src/components/pages/rating.js
+++ frontend/src/components/pages/rating.js
@@ -127,7 +127,7 @@ const RatingPage = () => {
             <FaStar
               clasName="star"
               size={100}
-              style={{ cursor: "pointer", paddingTop: '30px' }}
+              style={{ cursor: "pointer" }}
               color={ratingValue <= (ratings) ? "#ffc107" : "#e4e5e9"}
             />
           </label>
