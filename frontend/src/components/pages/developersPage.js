77d9ab2a326feb836b33033fd90ffa2c53fb6090
djpullino <djpullino6503@gmail.com>
2023-04-20 08:19:37 -0400

    Finishing off developersPage

--- frontend/src/components/pages/developersPage.js
+++ frontend/src/components/pages/developersPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { CloudFormation } from 'aws-sdk';
 
 var link = document.createElement('link');
 link.setAttribute('rel', 'stylesheet');
