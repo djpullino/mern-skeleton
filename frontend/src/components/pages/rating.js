c115786291217fcfb3e272fb255702fcf3c96c31
Nand2004 <nandp480@gmail.com>
2023-04-27 10:40:16 -0400

    Push Rating

--- frontend/src/components/pages/rating.js
+++ frontend/src/components/pages/rating.js
@@ -20,15 +20,16 @@ const RatingPage = () => {
   const [stationName, setStationName] = useState('');
   const [username, setUsername] = useState('');
 
+  const [user, setUser] = useState({})
 
 
   // For Handling and submitting the form.
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      console.log('handleSubmit: ', username, selectedLine, ratings, comments, Date);
+      console.log('handleSubmit: ', user.username, selectedLine, ratings, comments, Date);
       const response = await axios.post('http://localhost:8081/ratings/add', {
-        username: username,
+        username: user.username,
         stationName: selectedLine,
         ratings: ratings,
         comments: comments,
@@ -38,7 +39,7 @@ const RatingPage = () => {
       alert("Rating added successfully!");
     } catch (error) {
       console.error(error);
-      alert("Failed to add Rating. Please try again later.");
+      alert("Failed to add Rating. Please try again later.\n\nMake sure you are logged-in !");
     }
   };
 
@@ -59,15 +60,16 @@ const RatingPage = () => {
       setValidLines(validRoutes);
     }
     fetchRoutes();
+    setUser(getUserInfo( ))
   }, []);
   
 
-  // Another UseEffect for rendering all the ratings and showing it's history.
-  // function showHistory(stationName) {
-  //     if(stationName == selectedLine) {}
-  // }
-
   async function submit(event) {
+    if(!user.username) {
+      alert("No Logged in")
+      return
+    }
+  
     const result = await axios(`https://api-v3.mbta.com/stops?filter[route]=${selectedLine}`);
     const filteredStops = result.data.data.filter(stop => stop.attributes.wheelchair_boarding === 1);
     setStops(filteredStops);
@@ -114,12 +116,6 @@ const RatingPage = () => {
             {/* <button style={{ backgroundColor: '#cc5c99', color: 'white'}} onClick={submit} type="button">Enter</button> */}
           </div>
         </div>
-
-
-        <br></br>
-
-        <input type="text" placeholder="Enter your name" value={username} onChange={(event) => setUsername(event.target.value)} />
-
 
         <br></br>
         <br></br>
